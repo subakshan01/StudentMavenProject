db08327b1b4a7837c87687ac2f7e784863dae117
suba <subakshansivakumar07@gmail.com>
2026-09-22 18:28:47 +0530

    Initial Maven project

---
+++ src/test/java/mavenpractice/Testpractice.java
@@ -0,0 +1,11 @@
+package mavenpractice;
+import org.junit.jupiter.api.Test;
+
+
+public class Testpractice {
+	@Test
+	 void testAddition() {
+	        int result = 2 + 3;
+	        System.out.println(result);
+	    }
+}
